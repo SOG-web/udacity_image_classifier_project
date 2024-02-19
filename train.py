e620e5b99de1171e767cae7be125be4466d9ef68
Olalekan Raheem <raheemolalekanusman84@gmail.com>
2024-02-19 15:42:20 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,226 +1,137 @@
 import argparse
 import torch
 import torchvision
-from torch import nn, optim
-from torchvision import transforms
-from torchvision.datasets import ImageFolder
-from torch.utils import data
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
 import os
 
+def data_transformation(data_directory):
+    train_dir = os.path.join(data_directory, "train")
+    valid_dir = os.path.join(data_directory, "valid")
 
-def data_transformation(args):
-    train_dir = os.path.join(args.data_directory, "train")
-    valid_dir = os.path.join(args.data_directory, "valid")
-
-    # validate paths before doing anything else
-    if not os.path.exists(args.data_directory):
-        print("Data Directory doesn't exist: {}".format(args.data_directory))
+    if not os.path.exists(data_directory):
+        print(f"Data Directory doesn't exist: {data_directory}")
         raise FileNotFoundError
-    if not os.path.exists(args.save_directory):
-        print("Save Directory doesn't exist: {}".format(args.save_directory))
-        raise FileNotFoundError
-
     if not os.path.exists(train_dir):
-        print("Train folder doesn't exist: {}".format(train_dir))
+        print(f"Train folder doesn't exist: {train_dir}")
         raise FileNotFoundError
     if not os.path.exists(valid_dir):
-        print("Valid folder doesn't exist: {}".format(valid_dir))
+        print(f"Valid folder doesn't exist: {valid_dir}")
         raise FileNotFoundError
 
-    train_transforms = transforms.Compose([transforms.RandomRotation(30),
-                                           transforms.RandomResizedCrop(224),
-                                           transforms.RandomHorizontalFlip(),
-                                           transforms.ToTensor(),
-                                           transforms.Normalize([0.485, 0.456, 0.406], [
-                                                                0.229, 0.224, 0.225])
-                                           ])
+    train_transforms = transforms.Compose([
+        transforms.RandomRotation(30),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 
-    valid_transforms = transforms.Compose([transforms.Resize(255),
-                                           transforms.CenterCrop(224),
-                                           transforms.ToTensor(),
-                                           transforms.Normalize([0.485, 0.456, 0.406], [
-                                                                0.229, 0.224, 0.225])
-                                           ])
+    valid_transforms = transforms.Compose([
+        transforms.Resize(255),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 
-    train_data = ImageFolder(root=train_dir, transform=train_transforms)
-    valid_data = ImageFolder(root=valid_dir, transform=valid_transforms)
+    train_data = datasets.ImageFolder(root=train_dir, transform=train_transforms)
+    valid_data = datasets.ImageFolder(root=valid_dir, transform=valid_transforms)
 
-    train_data_loader = data.DataLoader(
-        train_data, batch_size=64, shuffle=True)
-    valid_data_loader = data.DataLoader(
-        valid_data, batch_size=64, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+    valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=64, shuffle=True)
 
-    return train_data_loader, valid_data_loader, train_data.class_to_idx
+    return train_loader, valid_loader, train_data.class_to_idx
 
+def train_model(data_directory, model_arch='vgg19', save_directory='../saved_models', learning_rate=0.003, epochs=3, gpu=False):
+    device = torch.device("cuda" if gpu and torch.cuda.is_available() else "cpu")
 
-def train_model(args, train_data_loader, valid_data_loader, class_to_idx):
-    if args.model_arch == "vgg11":
-        model = torchvision.models.vgg11(pretrained=True)
-    elif args.model_arch == "vgg13":
-        model = torchvision.models.vgg13(pretrained=True)
-    elif args.model_arch == "vgg16":
-        model = torchvision.models.vgg16(pretrained=True)
-    elif args.model_arch == "vgg16_bn":
-        model = torchvision.models.vgg16_bn(pretrained=True)
-    elif args.model_arch == "vgg19":
+    train_loader, valid_loader, class_to_idx = data_transformation(data_directory)
+
+    if model_arch == "vgg19":
         model = torchvision.models.vgg19(pretrained=True)
+    else:
+        model = getattr(torchvision.models, model_arch)(pretrained=True)
 
-    # freeze model parameters
     for param in model.parameters():
         param.requires_grad = False
 
-    in_features_of_pretrained_model = model.classifier[0].in_features
-
-    # alter the classifier so that it has 102 out features (i.e. len(cat_to_name.json))
-    classifier = nn.Sequential(nn.Linear(in_features=in_features_of_pretrained_model, out_features=2048, bias=True),
-                               nn.ReLU(inplace=True),
-                               nn.Dropout(p=0.2),
-                               nn.Linear(in_features=2048,
-                                         out_features=102, bias=True),
-                               nn.LogSoftmax(dim=1)
-                               )
-
+    in_features = model.classifier[0].in_features
+    classifier = nn.Sequential(
+        nn.Linear(in_features, 2048),
+        nn.ReLU(inplace=True),
+        nn.Dropout(p=0.2),
+        nn.Linear(2048, 102),
+        nn.LogSoftmax(dim=1)
+    )
     model.classifier = classifier
 
-    # specify criterion
     criterion = nn.NLLLoss()
-
-    # specify optimizer, using only classifer params. don't want to change the rest of VGG19
-    optimizer = optim.Adam(model.classifier.parameters(),
-                           lr=args.learning_rate)
-
-    # decide device depending on user arguments and device availability
-    if args.gpu and torch.cuda.is_available():
-        device = 'cuda'
-    elif args.gpu and not (torch.cuda.is_available()):
-        device = 'cpu'
-        print("GPU was selected as the training device, but no GPU is available. Using CPU instead.")
-    else:
-        device = 'cpu'
-    print("Using {} to train model.".format(device))
-
-    # move model to selected device
+    optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)
     model.to(device)
 
-    # init variables for tracking loss/steps etc.
     print_every = 20
 
-    # for each epoch
-    for e in range(args.epochs):
-        step = 0
+    for epoch in range(epochs):
         running_train_loss = 0
-        running_valid_loss = 0
-
-        # for each batch of images
-        for images, labels in train_data_loader:
-            step += 1
-
-            # turn model to train mode
+        for step, (images, labels) in enumerate(train_loader, 1):
             model.train()
-
-            # move images and model to device
             images, labels = images.to(device), labels.to(device)
-
-            # zeroise grad
             optimizer.zero_grad()
-
-            # forward
             outputs = model(images)
-
-            # loss
-            train_loss = criterion(outputs, labels)
-
-            # backward
-            train_loss.backward()
-
-            # step
+            loss = criterion(outputs, labels)
+            loss.backward()
             optimizer.step()
+            running_train_loss += loss.item()
 
-            running_train_loss += train_loss.item()
-
-            if step % print_every == 0 or step == 1 or step == len(train_data_loader):
-                print("Epoch: {}/{} Batch % Complete: {:.2f}%".format(e +
-                      1, args.epochs, (step)*100/len(train_data_loader)))
-
-        # validate
-        # turn model to eval mode
-        # turn on no_grad
+            if step % print_every == 0 or step == 1 or step == len(train_loader):
+                print(f"Epoch: {epoch+1}/{epochs} Batch % Complete: {step*100/len(train_loader):.2f}%")
 
         model.eval()
         with torch.no_grad():
             print("Validating Epoch....")
-            # for each batch of images
-            running_accuracy = 0
             running_valid_loss = 0
-            for images, labels in valid_data_loader:
-
-                # move images and model to device
+            running_accuracy = 0
+            for images, labels in valid_loader:
                 images, labels = images.to(device), labels.to(device)
-
-                # forward
                 outputs = model(images)
-
-                # loss
                 valid_loss = criterion(outputs, labels)
                 running_valid_loss += valid_loss.item()
-
-                # accuracy
                 ps = torch.exp(outputs)
                 top_p, top_class = ps.topk(1, dim=1)
                 equals = top_class == labels.view(*top_class.shape)
-                running_accuracy += torch.mean(
-                    equals.type(torch.FloatTensor)).item()
+                running_accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
 
-                # print stats
-            average_train_loss = running_train_loss/len(train_data_loader)
-            average_valid_loss = running_valid_loss/len(valid_data_loader)
-            accuracy = running_accuracy/len(valid_data_loader)
-            print("Train Loss: {:.3f}".format(average_train_loss))
-            print("Valid Loss: {:.3f}".format(average_valid_loss))
-            print("Accuracy: {:.3f}%".format(accuracy*100))
+            average_train_loss = running_train_loss/len(train_loader)
+            average_valid_loss = running_valid_loss/len(valid_loader)
+            accuracy = running_accuracy/len(valid_loader)
+            print(f"Train Loss: {average_train_loss:.3f}")
+            print(f"Valid Loss: {average_valid_loss:.3f}")
+            print(f"Accuracy: {accuracy*100:.3f}%")
 
     model.class_to_idx = class_to_idx
     checkpoint = {'classifier': model.classifier,
                   'state_dict': model.state_dict(),
-                  'epochs': args.epochs,
+                  'epochs': epochs,
                   'optim_stat_dict': optimizer.state_dict(),
                   'class_to_idx': model.class_to_idx,
-                  'vgg_type': args.model_arch
+                  'vgg_type': model_arch
                   }
 
-    torch.save(checkpoint, os.path.join(args.save_directory, "checkpoint.pth"))
-    print("model saved to {}".format(os.path.join(
-        args.save_directory, "checkpoint.pth")))
+    checkpoint_path = os.path.join(save_directory, "checkpoint.pth")
+    torch.save(checkpoint, checkpoint_path)
+    print(f"Model saved to {checkpoint_path}")
     return True
 
-
 if __name__ == '__main__':
-
-    # Create the parser and add arguments
     parser = argparse.ArgumentParser()
-
-    # required arguments
-    parser.add_argument(dest='data_directory',
-                        help="This is the dir of the training images e.g. if a sample file is in /flowers/train/daisy/001.png then supply /flowers. Expect 2 folders within, 'train' & 'valid'")
-
-    # optional arguments
-    parser.add_argument('--save_directory', dest='save_directory',
-                        help="This is the dir where the model will be saved after training.", default='../saved_models')
-    parser.add_argument('--learning_rate', dest='learning_rate',
-                        help="This is the learning rate when training the model. Default is 0.003. Expect float type", default=0.003, type=float)
-    parser.add_argument('--epochs', dest='epochs',
-                        help="This is the number of epochs when training the model. Default is 5. Expect int type", default=3, type=int)
-    parser.add_argument(
-        '--gpu', dest='gpu', help="Include this argument if you want to train the model on the GPU via CUDA", action='store_true')
-    parser.add_argument('--model_arch', dest='model_arch', help="This is type of pre-trained model that will be used",
-                        default="vgg19", type=str, choices=['vgg11', 'vgg13', 'vgg16', 'vgg19'])
-
-    # Parse and print the results
+    parser.add_argument('data_directory', help="Directory of the training images")
+    parser.add_argument('--model_arch', dest='model_arch', help="Type of pre-trained model to be used",
+                        default="vgg19", choices=['vgg11', 'vgg13', 'vgg16', 'vgg19'])
+    parser.add_argument('--save_directory', dest='save_directory', help="Directory where the model will be saved after training", default='../saved_models')
+    parser.add_argument('--learning_rate', dest='learning_rate', help="Learning rate when training the model. Default is 0.003", default=0.003, type=float)
+    parser.add_argument('--epochs', dest='epochs', help="Number of epochs when training the model. Default is 3", default=3, type=int)
+    parser.add_argument('--gpu', dest='gpu', help="Use GPU for training if available", action='store_true')
     args = parser.parse_args()
 
-    # load and transform data
-    train_data_loader, valid_data_loader, class_to_idx = data_transformation(
-        args)
-
-    # train and save model
-    train_model(args, train_data_loader, valid_data_loader, class_to_idx)
+    train_model(args.data_directory, args.model_arch, args.save_directory, args.learning_rate, args.epochs, args.gpu)
